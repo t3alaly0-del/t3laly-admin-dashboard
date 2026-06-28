070bb3872e9c4a46d173043074f13f68bc64f084
Yasmine Hussien <yasmiinehussien@gmail.com>
2026-06-28 20:09:08 +0300

    fix server path

--- build/web/server.js
+++ build/web/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'build/web')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'web', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build/web', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Admin dashboard running on port ${PORT}`));
+app.listen(PORT, () => console.log('Running on port ' + PORT));
