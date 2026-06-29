96a6204b1f14472d7521e2c9d7d3d43359a2272c
Yasmine Hussien <yasmiinehussien@gmail.com>
2026-06-29 04:03:12 +0300

    deploy railway

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "362978874" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1334809954" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
